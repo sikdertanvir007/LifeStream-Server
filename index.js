92202db1c61e5dfb2141702ccd9a984edbb462db
unknown <mehrabhasantanvir@gmail.com>
2025-07-25 02:53:49 +0600

    fb token verified

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const admin = require("firebase-admin");
 
 dotenv.config();
 
@@ -13,6 +14,17 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c0uxcug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -33,6 +45,40 @@ async function run() {
     const fundingsCollection = db.collection("fundings");
     const usersCollection = db.collection('users');
 
+
+//custom middlewares
+const verifyFBToken = async (req,res,next) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    const token = authHeader.split('')[1];
+    if(!token){
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+
+    //verify the token 
+
+    next();
+}
+
+
+
+
+app.post('/users',async(req,res)=>{
+    const email = req.body.email;
+    const userExists = await usersCollection.findOne({email})
+    if(userExists){
+        return res.status(200).send({message: 'User already exists', inserted:false});
+    } 
+    const user = req.body;
+    const result = await usersCollection.insertOne(user);
+    res.send(result);
+})
+
+
+
+
     // ✅ GET all donation requests
     app.get("/donation-requests", async (req, res) => {
       const result = await donationRequestsCollection.find().toArray();
@@ -51,7 +97,7 @@ async function run() {
     });
 
     // ✅ GET donation requests made by a specific user (with optional status filter & pagination)
-    app.get("/my-donation-requests", async (req, res) => {
+    app.get("/my-donation-requests",verifyFBToken, async (req, res) => {
       try {
         const { email, status, page = 1, limit = 5 } = req.query;
 
